db2e30bc166dd9ff4be92141efa9a075ab2376af
aaronjan <aaronjan98@gmail.com>
2020-11-04 07:15:25 -0800

    Create Main.java

---
+++ src/javaSnackBar/Main.java
@@ -0,0 +1,14 @@
+package javaSnackBar;
+
+public class Main
+{
+	private static void workWithData()
+	{
+		System.out.println("working");
+	}
+
+	public static void main(String[] args)
+	{
+		workWithData();
+	}
+}
